f295d3ee5e3b4046f745fa079e8b4cd84b4cb32d
Dmeraks <dmeraks@users.noreply.github.com>
2026-09-04 11:40:01 -0500

    fix: remove unused useEffect import in BackupPanel

--- web/src/features/settings/BackupPanel.tsx
+++ web/src/features/settings/BackupPanel.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react'
+import { useCallback, useState } from 'react'
 import { useQuery, useQueryClient } from '@tanstack/react-query'
 import { api } from '../../api/client'
 import { useConfirm } from '../../components/confirm/ConfirmProvider'
